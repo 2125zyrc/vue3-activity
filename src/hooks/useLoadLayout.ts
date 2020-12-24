ccfd99a7a92c7b906d3a84818db2c2695e7eace3
yangruichao <283668770@qq.com>
2020-12-24 17:07:42 +0800

    hook

--- src/hooks/useLoadLayout.ts
+++ src/hooks/useLoadLayout.ts
@@ -1,33 +1,9 @@
 import {  defineAsyncComponent } from "vue";
-import path from 'path';
-
-const modulesFiles:any = require.context("@/components/layout", true, /\.vue$/);
-// console.log(path)
-interface itemRow {
-  name: string,
-  pathStr: string
-}
-
-const modules =  modulesFiles.keys().reduce((modules:Array<itemRow>, modulePath:string):Array<itemRow> => {
-  const name = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1');
-  // const pathStr = path.join('/','/')
-  modulePath = modulePath.split('./')[1]
-  const item:itemRow = {
-    name,
-    pathStr: `@/components/layout/${modulePath}`
-  }
-  return modules.concat(item)
-}, []);
-
-console.log(modules)
 
 const useLoadLayout =()=>{
-  let layout:any = { };
-  modules.map((item: itemRow)=>{
-    const x = '@/components/layout/GoodsList.vue'
-    layout[item.name] = defineAsyncComponent(()=>import(x))
-  })
-  return layout
+  return {
+    GoodsList: defineAsyncComponent(()=>import('@/components/layout/GoodsList.vue'))
+  }
 }
 
 export default useLoadLayout
