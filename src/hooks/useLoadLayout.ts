8df1be22ea07e4815dc11d71b60fab7c1e24600b
yangruichao <283668770@qq.com>
2020-12-25 10:44:32 +0800

    测试

--- src/hooks/useLoadLayout.ts
+++ src/hooks/useLoadLayout.ts
@@ -1,5 +1,8 @@
 import {  defineAsyncComponent } from "vue";
 
+import * as path from 'path'
+console.log(path);
+
 const useLoadLayout =()=>{
   return {
     GoodsList: defineAsyncComponent(()=>import('@/components/layout/GoodsList.vue'))
